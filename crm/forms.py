0a092873164dbe7b5b83820e0def577787bb2eb6
santillo515 <santillo515@mail.ru>
2022-05-13 00:34:13 +0700

    project_commit

--- crm/forms.py
+++ crm/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class OrderForm(forms.Form):
-    name = forms.CharField(max_length=280, required=False, widget=forms.TextInput(attrs={'class': 'css_input'}))
-    phone = forms.CharField(max_length=280)
+    name = forms.CharField(max_length=280, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone = forms.CharField(max_length=280, widget=forms.TextInput(attrs={'class': 'form-control'}))
